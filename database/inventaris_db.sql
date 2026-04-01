f0757ecc79a4a1507d350a64f497cbf0e208f395
Nekuro_25 <febriancahaya25@gmail.com>
2026-04-01 21:00:25 +0700

    tambah database inventaris

---
+++ database/inventaris_db.sql
@@ -0,0 +1,187 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 01 Apr 2026 pada 15.58
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `inventaris_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `inventaris`
+--
+
+CREATE TABLE `inventaris` (
+  `id_barang` int(11) NOT NULL,
+  `kode_barang` varchar(50) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `jenis` enum('PC','Laptop') NOT NULL,
+  `merk` varchar(100) DEFAULT NULL,
+  `processor` varchar(100) DEFAULT NULL,
+  `ram` varchar(50) DEFAULT NULL,
+  `storage` varchar(50) DEFAULT NULL,
+  `id_lokasi` int(11) DEFAULT NULL,
+  `status` enum('aktif','rusak','maintenance') DEFAULT 'aktif'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `inventaris`
+--
+
+INSERT INTO `inventaris` (`id_barang`, `kode_barang`, `nama_barang`, `jenis`, `merk`, `processor`, `ram`, `storage`, `id_lokasi`, `status`) VALUES
+(6, '001/LENOVO450', 'Lenovo 001', 'PC', 'Lenovo', 'Intel Core 2 ', '2 GB', '256 GB', 5, 'rusak');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `lokasi`
+--
+
+CREATE TABLE `lokasi` (
+  `id_lokasi` int(11) NOT NULL,
+  `nama_lokasi` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `lokasi`
+--
+
+INSERT INTO `lokasi` (`id_lokasi`, `nama_lokasi`) VALUES
+(5, 'Lab 2');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `perbaikan`
+--
+
+CREATE TABLE `perbaikan` (
+  `id_perbaikan` int(11) NOT NULL,
+  `id_barang` int(11) DEFAULT NULL,
+  `tanggal` date DEFAULT NULL,
+  `kerusakan` text DEFAULT NULL,
+  `tindakan` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `perbaikan`
+--
+
+INSERT INTO `perbaikan` (`id_perbaikan`, `id_barang`, `tanggal`, `kerusakan`, `tindakan`) VALUES
+(1, 6, '2026-03-17', 'lcd error', 'diperbaiki teknisi pp');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('admin','teknisi','user') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id_user`, `nama`, `username`, `password`, `role`) VALUES
+(2, 'tes', 'tes', 'tes', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `inventaris`
+--
+ALTER TABLE `inventaris`
+  ADD PRIMARY KEY (`id_barang`),
+  ADD KEY `id_lokasi` (`id_lokasi`);
+
+--
+-- Indeks untuk tabel `lokasi`
+--
+ALTER TABLE `lokasi`
+  ADD PRIMARY KEY (`id_lokasi`);
+
+--
+-- Indeks untuk tabel `perbaikan`
+--
+ALTER TABLE `perbaikan`
+  ADD PRIMARY KEY (`id_perbaikan`),
+  ADD KEY `id_barang` (`id_barang`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `inventaris`
+--
+ALTER TABLE `inventaris`
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `lokasi`
+--
+ALTER TABLE `lokasi`
+  MODIFY `id_lokasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `perbaikan`
+--
+ALTER TABLE `perbaikan`
+  MODIFY `id_perbaikan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `inventaris`
+--
+ALTER TABLE `inventaris`
+  ADD CONSTRAINT `inventaris_ibfk_1` FOREIGN KEY (`id_lokasi`) REFERENCES `lokasi` (`id_lokasi`);
+
+--
+-- Ketidakleluasaan untuk tabel `perbaikan`
+--
+ALTER TABLE `perbaikan`
+  ADD CONSTRAINT `perbaikan_ibfk_1` FOREIGN KEY (`id_barang`) REFERENCES `inventaris` (`id_barang`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

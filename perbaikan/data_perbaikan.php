f1166b9fda0cf9d56da69cb197997f0d556a4d1e
Nekuro_25 <febriancahaya25@gmail.com>
2026-04-03 01:56:35 +0700

    rapikan data perbaikan

--- perbaikan/data_perbaikan.php
+++ perbaikan/data_perbaikan.php
@@ -31,95 +31,80 @@ JOIN inventaris ON perbaikan.id_barang = inventaris.id_barang
 
 <div class="container">
 
-<!-- Sidebar -->
-<div class="sidebar">
+    <!-- Sidebar -->
+    <div class="sidebar">
 
-<h2>Inventaris</h2>
+        <h2>Inventaris</h2>
 
-<ul>
-<li><a href="../dashboard/index.php">Dashboard</a></li>
-<li><a href="../inventaris/data.php">Data Inventaris</a></li>
-<li><a href="../lokasi/lokasi.php">Data Lokasi</a></li>
-<li><a href="data_perbaikan.php">Perbaikan</a></li>
-<li><a href="../laporan/laporan.php">Laporan</a></li>
-<li><a href="../user/data_user.php">Manajemen User</a></li>
-<li><a href="../logout.php">Logout</a></li>
-</ul>
+        <ul>
+            <li><a href="../dashboard/index.php">Dashboard</a></li>
+            <li><a href="../inventaris/data.php">Data Inventaris</a></li>
+            <li><a href="../lokasi/lokasi.php">Data Lokasi</a></li>
+            <li><a href="data_perbaikan.php">Perbaikan</a></li>
+            <li><a href="../laporan/laporan.php">Laporan</a></li>
+            <li><a href="../user/data_user.php">Manajemen User</a></li>
+            <li><a href="../logout.php">Logout</a></li>
+        </ul>
 
-</div>
+    </div>
 
-<!-- Main Content -->
-<div class="main">
+    <!-- Main Content -->
+    <div class="main">
 
-<div class="topbar">
-<h1>Data Perbaikan</h1>
-</div>
+        <div class="topbar">
+            <h1>Data Perbaikan</h1>
+        </div>
 
-<div class="table-container">
+        <div class="table-container">
 
-<a href="tambah_perbaikan.php" class="btn-tambah">+ Tambah Perbaikan</a>
+            <a href="tambah_perbaikan.php" class="btn-tambah">+ Tambah Perbaikan</a>
 
-<table>
+            <table>
 
-<thead>
-<tr>
-<th>No</th>
-<th>Kode Barang</th>
-<th>Nama Barang</th>
-<th>Tanggal</th>
-<th>Kerusakan</th>
-<th>Tindakan</th>
-<th>Aksi</th>
-</tr>
-</thead>
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Kode Barang</th>
+                        <th>Nama Barang</th>
+                        <th>Tanggal</th>
+                        <th>Kerusakan</th>
+                        <th>Tindakan</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
 
-<tbody>
+                <tbody>
 
-<?php
-$no = 1;
+                    <?php
+                    $no = 1;
 
-while($row = mysqli_fetch_assoc($query)){
-?>
+                    while($row = mysqli_fetch_assoc($query)){
+                    ?>
 
-<tr>
+                    <tr>
 
-<td><?php echo $no++; ?></td>
+                        <td><?php echo $no++; ?></td>
+                        <td><?php echo $row['kode_barang']; ?></td>
+                        <td><?php echo $row['nama_barang']; ?></td>
+                        <td><?php echo $row['tanggal']; ?></td>
+                        <td><?php echo $row['kerusakan']; ?></td>
+                        <td><?php echo $row['tindakan']; ?></td>
+                        <td>
+                            <a href="edit_perbaikan.php?id=<?php echo $row['id_perbaikan']; ?>" class="btn-edit">Edit</a>
+                            <a href="hapus_perbaikan.php?id=<?php echo $row['id_perbaikan']; ?>" class="btn-hapus"onclick="return konfirmasiHapus('Yakin ingin menghapus data ini?')">Hapus</a>
+                        </td>
+                    
+                    </tr>
 
-<td><?php echo $row['kode_barang']; ?></td>
+                    <?php } ?>
 
-<td><?php echo $row['nama_barang']; ?></td>
+                </tbody>
 
-<td><?php echo $row['tanggal']; ?></td>
+            </table>
 
-<td><?php echo $row['kerusakan']; ?></td>
+        </div>
 
-<td><?php echo $row['tindakan']; ?></td>
-
-<td>
-
-<a href="edit_perbaikan.php?id=<?php echo $row['id_perbaikan']; ?>" class="btn-edit">
-Edit
-</a>
-
-<a href="hapus_perbaikan.php?id=<?php echo $row['id_perbaikan']; ?>" 
-class="btn-hapus"
-onclick="return konfirmasiHapus('Yakin ingin menghapus data ini?')">
-Hapus
-</a>
-
-</td>
-
-</tr>
-
-<?php } ?>
-
-</tbody>
-
-</table>
-
-</div>
-
-</div>
+    </div>
 
 </div>
 
